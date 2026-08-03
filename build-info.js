8b7a017a85176b7dc5c74239beabef274b0c60a2
SeokMinKo <36186545+SeokMinKo@users.noreply.github.com>
2026-08-03 17:06:51 +0900

    v1.6.2: update build-info.js

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 'use strict';
 globalThis.__MOE_SSD_BUILD__ = Object.freeze({
   schemaVersion: 'moe-ssd-sim/v4',
-  modelVersion: '1.6.1-candidate',
-  packageVersion: '1.6.1',
+  modelVersion: '1.6.2',
+  packageVersion: '1.6.2',
   commit: 'development',
   buildVersion: 'source-tree'
 });
